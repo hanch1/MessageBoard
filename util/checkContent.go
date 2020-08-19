c058f9b56371cf9236da73328710b1f49bee5dab
赵含驰917 <240813605@qq.com>
2020-08-19 11:33:39 +0800

    关键词屏蔽功能

---
+++ util/checkContent.go
@@ -0,0 +1,114 @@
+package util
+
+import "unicode/utf8"
+
+/*
+  字符串过滤 （前缀树实现）
+*/
+type Trie struct {
+	Root *TrieNode
+}
+
+type TrieNode struct {
+	Children map[rune]*TrieNode
+	End      bool
+}
+
+func NewTrie() Trie {
+	var r Trie
+	r.Root = NewTrieNode()
+	return r
+}
+
+func NewTrieNode() *TrieNode {
+	n := new(TrieNode)
+	n.Children = make(map[rune]*TrieNode)
+	return n
+}
+
+func (this *Trie) Insert(txt string) {
+	if len(txt) < 1 {
+		return
+	}
+	node := this.Root
+	key := []rune(txt)
+	for i := 0; i < len(key); i++ {
+		if _, exists := node.Children[key[i]]; !exists {
+			node.Children[key[i]] = NewTrieNode()
+		}
+		node = node.Children[key[i]]
+	}
+
+	node.End = true
+}
+
+func (this *Trie) IsExist(txt string) bool {
+	if len(txt) < 1 {
+		return false
+	}
+	node := this.Root
+	key := []rune(txt)
+	var chars []rune = nil
+	slen := len(key)
+	for i := 0; i < slen; i++ {
+		if _, exists := node.Children[key[i]]; exists {
+			node = node.Children[key[i]]
+			for j := i + 1; j < slen; j++ {
+				if _, exists := node.Children[key[j]]; exists {
+					node = node.Children[key[j]]
+					if node.End == true {
+						if chars == nil {
+							chars = key
+						}
+						for t := i; t <= j; t++ {
+							return true
+						}
+						i = j
+						node = this.Root
+						break
+					}
+				}
+			}
+			node = this.Root
+		}
+	}
+	return false
+}
+
+func (this *Trie) Replace(txt string) string {
+	if len(txt) < 1 {
+		return txt
+	}
+	node := this.Root
+	key := []rune(txt)
+	var chars []rune = nil
+	slen := len(key)
+	for i := 0; i < slen; i++ {
+		if _, exists := node.Children[key[i]]; exists {
+			node = node.Children[key[i]]
+			for j := i + 1; j < slen; j++ {
+				if _, exists := node.Children[key[j]]; exists {
+					node = node.Children[key[j]]
+					if node.End == true {
+						if chars == nil {
+							chars = key
+						}
+						for t := i; t <= j; t++ {
+							c, _ := utf8.DecodeRuneInString("*")
+							chars[t] = c
+						}
+						i = j
+						node = this.Root
+						break
+					}
+				}
+			}
+			node = this.Root
+		}
+	}
+	if chars == nil {
+		return txt
+	} else {
+		return string(chars)
+	}
+}
